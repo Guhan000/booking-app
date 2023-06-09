62da8c2373939ba6885b967b6ff76501b168f7d2
Guhan <guhan27062002@gmail.com>
2023-06-09 16:48:03 +0530

    react fetch using URL location

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import './navbar.css'
+import './navbar.css';
+import { Link } from 'react-router-dom';
 
 const navbar = () => {
   return (
     <div className='navbar'>
         <div className="nav-container">
+          <Link to="/" style={{color: "inherit", textDecoration:null}}>
           <div className="logo">Booking.com</div>
+          </Link>
           <div className="nav-buttons">
             <button className='reg-button'>Register</button>
             <button className='login-button'>Login</button>
